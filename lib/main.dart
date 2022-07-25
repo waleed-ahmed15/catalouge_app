565e41ad169eff03890db8b329392b6be3a867e7
waleed-ahmed15 <waleed.ah755@gmail.com>
2022-07-25 22:38:58 +0500

    day 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(child: Text("hello there")),
+    );
   }
 }
