04921647f4ec87d50bafeaccdc3ce6cf7fe03f38
waleed-ahmed15 <waleed.ah755@gmail.com>
2022-07-30 19:56:32 +0500

    day 10 completed

---
+++ lib/models/calatoge.dart
@@ -0,0 +1,15 @@
+class Item {
+  String id = "";
+  String name = "";
+  String desc = "";
+  num price = 0;
+  String color = "";
+  String image = "";
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
