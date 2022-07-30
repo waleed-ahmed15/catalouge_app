4816137b8e0d234f839ced2e9882af0741bda003
waleed-ahmed15 <waleed.ah755@gmail.com>
2022-07-30 21:46:59 +0500

    day 12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:catalouge_app/models/calatoge.dart';
 import 'package:catalouge_app/widgets/drawer.dart';
+import 'package:catalouge_app/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
 
 class Home_page extends StatelessWidget {
@@ -8,10 +10,12 @@ class Home_page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text("Catalouge App"))),
-      body: Container(
-          child: Center(
-        child: Text("welcome"),
-      )),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: ((context, index) {
+          return ItemWidget(item: CatalogModel.items[0]);
+        }),
+      ),
       drawer: MyDrawer(),
     );
   }
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,27 @@
+import 'package:catalouge_app/models/calatoge.dart';
+import 'package:flutter/material.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+  const ItemWidget({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListTile(
+          onTap: () {
+            print("${item.name}");
+          },
+          leading: Image(
+            image: NetworkImage(item.image),
+          ),
+          title: Text(item.name),
+          subtitle: Text(item.desc),
+          trailing: Text("\$${item.price.toString()}"),
+        ),
+      ),
+    );
+  }
+}
