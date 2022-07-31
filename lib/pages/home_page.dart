d5fe81d6b09f3c4a531fa7344c0c7b5660d8beb8
waleed-ahmed15 <waleed.ah755@gmail.com>
2022-07-31 18:13:25 +0500

    day 15 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -39,11 +39,34 @@ class _Home_pageState extends State<Home_page> {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text("Catalouge App"))),
       body: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-          ? ListView.builder(
+          ? GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, mainAxisSpacing: 16, crossAxisSpacing: 16),
+              itemBuilder: (context, index) {
+                final itm = CatalogModel.items[index];
+                return Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: GridTile(
+                    header: Container(
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(color: Colors.deepPurple),
+                        child: Text(
+                          itm.name,
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        )),
+                    child: Image.network(itm.image),
+                    footer: Container(
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(color: Colors.deepPurple),
+                        child: Text(itm.price.toString(),
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 15))),
+                  ),
+                );
+              },
               itemCount: CatalogModel.items.length,
-              itemBuilder: ((context, index) {
-                return ItemWidget(item: CatalogModel.items[index]);
-              }),
             )
           : Center(
               child: CircularProgressIndicator(),
